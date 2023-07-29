b8e0e2be4272bc796df3c30466db5799e5684166
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-07-29 17:28:13 +0530

    project completed

--- src/remove_.py
+++ src/remove_.py
@@ -3,15 +3,12 @@ import streamlit as st
 
 def remove():
     st.markdown(""" 
-    <style>
-
-    .css-erpbzb.edgvbvh3
-    {
-    visibility:hidden;
-    }
-
-    </style>
-    """,unsafe_allow_html=True)
+        <style>
+        .css-10pw50 {
+            display: none;
+        }
+        </style>
+    """, unsafe_allow_html=True)
 
     st.markdown(""" 
     <style>
