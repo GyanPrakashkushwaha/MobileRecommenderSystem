812e76b8ac8409ab8e610f662007cb567deb4255
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-07-29 15:31:33 +0530

    src

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pickle
 import requests
-# from recommender.designs.others.remove_ import remove
+from src.remove_ import remove
 
 st.set_page_config(page_title="Movie Recommender System", page_icon=":📲:", layout="wide", initial_sidebar_state="expanded")
 
+# remove()
 
 
 
---
+++ src/remove_.py
@@ -0,0 +1,24 @@
+
+import streamlit as st
+
+def remove():
+    st.markdown(""" 
+    <style>
+
+    .css-erpbzb.edgvbvh3
+    {
+    visibility:hidden;
+    }
+
+    </style>
+    """,unsafe_allow_html=True)
+
+    st.markdown(""" 
+    <style>
+
+    .css-10pw50.egzxvld1
+    {
+    visibility:hidden;
+    }
+    </style>
+    """,unsafe_allow_html=True)
