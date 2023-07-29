54b529f074e922e074acf0fb811443a9855dc7be
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-07-29 18:17:45 +0530

    src

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from src.remove_ import remove
 
 st.set_page_config(page_title="Mobile Recommender System", page_icon=":📲:", layout="wide", initial_sidebar_state="expanded")
 
-df = pickle.load(file=open(file=r'src\model\dataframe.pkl', mode='rb'))
-similarity = pickle.load(file=open(file=r'src\model\similarity.pkl', mode='rb'))
+df = pickle.load(file=open(file=r'src/model/dataframe.pkl', mode='rb'))
+similarity = pickle.load(file=open(file=r'src/model/similarity.pkl', mode='rb'))
 
 
 remove()
