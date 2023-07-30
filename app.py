56179c25829abc4f2d6f53fa1c71bf85bbaa42a2
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-07-30 11:41:26 +0530

    app.py

--- app.py
+++ app.py
@@ -199,4 +199,4 @@ if st.button('Recommend'):
         st.image(mobile_IMG_variety[9])
 
 st.markdown('---')
-st.markdown('> ###Thank You for visiting🙂 Made by👨🏻‍💻 Gyan Prakash Kushwaha')
+st.markdown('> #### Thank You for visiting🙂 \n Made by👨🏻‍💻 Gyan Prakash Kushwaha')
